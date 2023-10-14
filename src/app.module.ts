59c3257024a03b86d0d102ecb8b5f0ffa1359179
Youki YOSHIOKA <yotch.youki.you@gmail.com>
2023-10-14 11:30:16 +0900

    implemented swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,14 @@
 import { Module } from '@nestjs/common'
-import { AzureCosmosDbModule } from '@nestjs/azure-database'
+import { ConfigModule } from '@nestjs/config'
+import { CardModule } from '~/card/card.module'
+import { CosmosClientProvider } from '~/providers/cosmosClient.provider'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { ConfigModule } from '@nestjs/config'
 
-console.info('process.env.COSMOS_DB_NAME :', process.env.COSMOS_DB_NAME)
-console.info('process.env.COSMOS_DB_ENDPOINT :', process.env.COSMOS_DB_ENDPOINT)
-console.info('process.env.COSMOS_DB_KEY :', process.env.COSMOS_DB_KEY)
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env'],
-    }),
-    AzureCosmosDbModule.forRoot({
-      dbName: process.env.COSMOS_DB_NAME ?? '',
-      endpoint: process.env.COSMOS_DB_ENDPOINT ?? '',
-      key: process.env.COSMOS_DB_KEY,
-    }),
-  ],
+  imports: [ConfigModule.forRoot(), CardModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-  ],
+  providers: [AppService, CosmosClientProvider],
+  exports: [CosmosClientProvider],
 })
 export class AppModule {}
